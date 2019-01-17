da57f6c49d03d02e71dbcad45f173deeda7d6d91
Sergio Nunez <checonunez@gmail.com>
2019-01-16 19:28:42 -0600

    master:Replacing project with current project, relocated to gitHub file.

--- iPark/ViewController.swift
+++ iPark/ViewController.swift
@@ -2,20 +2,18 @@
 //  ViewController.swift
 //  iPark
 //
-//  Created by Sergio Nunez on 12/17/18.
-//  Copyright © 2018 Sergio Nunez. All rights reserved.
+//  Created by Sergio Nunez on 1/16/19.
+//  Copyright © 2019 Sergio Nunez. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    
 
 
 }
